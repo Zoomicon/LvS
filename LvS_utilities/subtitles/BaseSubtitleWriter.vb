9d108293893e4c4517fc3211664472bd8f67e843
George Birbilis <birbilis@users.noreply.github.com>
2012-10-13 04:18:53 +0300

    LvS 4.5.5 - removed unused references

--- LvS_utilities/subtitles/BaseSubtitleWriter.vb
+++ LvS_utilities/subtitles/BaseSubtitleWriter.vb
@@ -8,35 +8,35 @@ Imports LvS.models.subtitles
 
 Namespace LvS.utilities.subtitles
 
- Public MustInherit Class BaseSubtitleWriter
-  Implements ISubtitlesWriter
+  Public MustInherit Class BaseSubtitleWriter
+    Implements ISubtitlesWriter
 
 #Region "Methods"
 
-		Public Overloads Sub WriteSubtitles(ByVal subtitles As models.subtitles.ISubtitles, ByVal path As String, ByVal theEncoding As Encoding) Implements models.subtitles.ISubtitlesWriter.WriteSubtitles
-			Using writer As New StreamWriter(path, False, theEncoding)
-				WriteSubtitles(subtitles, writer)
-			End Using
-		End Sub
+    Public Overloads Sub WriteSubtitles(ByVal subtitles As models.subtitles.ISubtitles, ByVal path As String, ByVal theEncoding As Encoding) Implements models.subtitles.ISubtitlesWriter.WriteSubtitles
+      Using writer As New StreamWriter(path, False, theEncoding)
+        WriteSubtitles(subtitles, writer)
+      End Using
+    End Sub
 
-  Public Overloads Sub WriteSubtitles(ByVal subtitles As models.subtitles.ISubtitles, ByVal writer As TextWriter)
-   For Each s As ISubtitle In subtitles
-    WriteSubtitle(s, writer)
-   Next
-  End Sub
+    Public Overloads Sub WriteSubtitles(ByVal subtitles As models.subtitles.ISubtitles, ByVal writer As TextWriter)
+      For Each s As ISubtitle In subtitles
+        WriteSubtitle(s, writer)
+      Next
+    End Sub
 
-  Protected Overridable Sub WriteHeader()
-   'can override at descendents
-  End Sub
+    Protected Overridable Sub WriteHeader()
+      'can override at descendents
+    End Sub
 
-  Protected Overridable Sub WriteFooter()
-   'can override at descendents
-  End Sub
+    Protected Overridable Sub WriteFooter()
+      'can override at descendents
+    End Sub
 
-  Protected MustOverride Sub WriteSubtitle(ByVal subtitle As ISubtitle, ByVal writer As TextWriter)
+    Protected MustOverride Sub WriteSubtitle(ByVal subtitle As ISubtitle, ByVal writer As TextWriter)
 
 #End Region
 
- End Class
+  End Class
 
 End Namespace
